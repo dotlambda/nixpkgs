7df116852819ac923643fd227d64f543b04f69d9
Robert Schütz <dev@schuetz-co.de>
2021-05-18 17:49:15 +0200

    python3Packages.ytmusicapi: 0.16.0 -> 0.17.0
    https://github.com/sigma67/ytmusicapi/releases/tag/0.17.0

--- pkgs/development/python-modules/ytmusicapi/default.nix
+++ pkgs/development/python-modules/ytmusicapi/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "ytmusicapi";
-  version = "0.16.0";
+  version = "0.17.0";
 
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-/94/taeBI6xZ3uN/wfMnk/NPmk+j0+aaH8CAZBEsK10=";
+    sha256 = "sha256-BM/akShcl4hzVf+H5zWhJZo+EPHeiZJW+0kNI8b3TU4=";
   };
 
   propagatedBuildInputs = [
